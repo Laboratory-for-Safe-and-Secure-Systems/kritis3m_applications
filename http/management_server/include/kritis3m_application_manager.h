458c463fe7142777381043d80c3419eba25686a6
scp38901 <philipp.schwind@oth-regensburg.de>
2024-10-08 13:12:49 +0200

    aplication manager iface

--- http/management_server/include/kritis3m_application_manager.h
+++ http/management_server/include/kritis3m_application_manager.h
@@ -1,12 +1,13 @@
 #ifndef KRITIS3M_APPLICATION_MANAGER_H
 #define KRITIS3M_APPLICATION_MANAGER_H
 #include "poll_set.h"
-
 #include "kritis3m_configuration.h"
 #include "tls_proxy.h"
 #include "echo_server.h"
 #include "tcp_client_stdin_bridge.h"
 #include "network_tester.h"
+
+#define GLOBAL_MANAGEMENT
 
 /***
  * includes threading
@@ -28,7 +29,14 @@ enum application_management_message_type
     APPLICATION_SERVICE_STATUS_REQUEST,
 
     APPLICATION_CONNECTION_REQUEST,
-    RESPONSE,
+    MSG_RESPONSE,
+};
+enum MSG_RESPONSE_CODE
+{
+    MSG_ERROR = -1,
+    MSG_OK = 0,
+    MSG_FORBIDDEN = 1,
+    MSG_BUSY = 2,
 };
 
 typedef struct connection_request
@@ -39,8 +47,6 @@ typedef struct connection_request
 
 typedef struct application_status
 {
-    int application_id;
-    Kritis3mApplicationtype type;
     bool is_running;
     union concrete_application_status
     {
@@ -62,23 +68,38 @@ typedef struct application_config
         proxy_config proxy_config;
         network_tester_config network_tester_config;
         tcp_client_stdin_bridge_config stdin_bridge_config;
+        ApplicationConfiguration application_configuration;
     } config;
 } application_config;
+
+typedef struct client_connection_request
+{
+    struct sockaddr_in client;
+    int application_id;
+}client_connection_request;
 
 typedef struct application_message
 {
     enum application_management_message_type msg_type;
+    Kritis3mApplicationtype type;
     union application_management_message_payload
     {
-        application_config config;
+
+        ApplicationConfiguration *config;
+        Kritis3mApplications *kritis3m_applicaiton;
+        application_config appl_config;
         application_status status_request;
+        client_connection_request client_con_request;
         int application_id;
+        struct sockaddr_in client;
+        enum MSG_RESPONSE_CODE return_code;
     } payload;
-    Kritis3mApplicationtype appl_type;
 } application_message;
 
-int init_application_manager(application_manager_config *config);
 int start_application_manager(ApplicationConfiguration *configuration);
+
+bool confirm_client(int application_id, struct sockaddr_in *connecting_client);
+
 int stop_application_manager();
 int terminate_application_manager();
 
